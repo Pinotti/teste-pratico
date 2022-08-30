f4a2f1099fd86ac82bcb56ec5d6c95c19ab0ef53
Anderson <anderson.pinotti@gmail.com>
2022-08-29 21:09:04 -0300

    Commit inicial e criação das migrations

---
+++ database/migrations/2022_08_29_234604_create_bolos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bolos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->nullable(false);
+            $table->integer('peso')->nullable(false);
+            $table->decimal('valor', 5, 2)->nullable(false);
+            $table->integer('quantidade')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bolos');
+    }
+};
---
+++ database/migrations/2022_08_30_000708_create_bolo_clientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bolo_clientes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('bolo_id')->references('id')->on('bolos');
+            $table->unsignedBigInteger('cliente_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bolo_clientes');
+    }
+};
